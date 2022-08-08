84c1df18930cad45a8bf9031a83a08dbfb69913f
kicker <thisissahupiyush@gmail.com>
2022-08-08 23:38:54 +0530

    UI Updated

--- static/index.js
+++ static/index.js
@@ -1,16 +1,20 @@
 function openForm1() {
-  document.getElementById("myForm1").style.display = "block";
-  document.getElementById("myForm2").style.display = "none";
-}
-
-function closeForm1() {
-  document.getElementById("myForm1").style.display = "none";
-}
-function openForm2() {
-	document.getElementById("myForm1").style.display = "none";
-  document.getElementById("myForm2").style.display = "block";
-}
-
-function closeForm2() {
-  document.getElementById("myForm2").style.display = "none";
-}
+    document.getElementById("myForm1").style.display = "block";
+    document.getElementById("myForm2").style.display = "none";
+    document.getElementById("mycontainer").style.filter = "blur(5px)";
+  }
+  
+  function closeForm1() {
+    document.getElementById("myForm1").style.display = "none";
+    document.getElementById("mycontainer").style.filter = "blur(0)";
+  }
+  function openForm2() {
+      document.getElementById("myForm1").style.display = "none";
+    document.getElementById("myForm2").style.display = "block";
+    document.getElementById("mycontainer").style.filter = "blur(5px)";
+  }
+  
+  function closeForm2() {
+    document.getElementById("myForm2").style.display = "none";
+    document.getElementById("mycontainer").style.filter = "blur(0)";
+  }

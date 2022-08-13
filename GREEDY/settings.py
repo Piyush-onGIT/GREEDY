8b3b78a5ee7352cfc463a96d75d53016a2b86e1c
kicker <thisissahupiyush@gmail.com>
2022-08-13 13:33:28 +0530

    ready to deploy

--- GREEDY/settings.py
+++ GREEDY/settings.py
@@ -51,7 +51,7 @@ MIDDLEWARE = [
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
 ]
 
-ROOT_URLCONF = 'DSA.urls'
+ROOT_URLCONF = 'GREEDY.urls'
 
 TEMPLATES = [
     {
@@ -69,7 +69,7 @@ TEMPLATES = [
     },
 ]
 
-WSGI_APPLICATION = 'DSA.wsgi.application'
+WSGI_APPLICATION = 'GREEDY.wsgi.application'
 
 
 # Database

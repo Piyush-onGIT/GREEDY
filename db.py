d4badca12e8ee616ba230609bfc681fbb59b99a6
kicker <thisissahupiyush@gmail.com>
2022-08-13 13:20:48 +0530

    procfile added

--- db.py
+++ db.py
@@ -4,8 +4,6 @@ import gspread
 from oauth2client.service_account import ServiceAccountCredentials
 from datetime import datetime
 
-# LF
-
 scope = ["https://spreadsheets.google.com/feeds", 'https://www.googleapis.com/auth/spreadsheets',
             "https://www.googleapis.com/auth/drive.file", "https://www.googleapis.com/auth/drive"]
 creds = ServiceAccountCredentials.from_json_keyfile_name("dsa.json", scope)

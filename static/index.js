808382cce448334133e6e2b759a3134dde5f1c2d
Anurag Deo <anuragjee2020@gmail.com>
2022-10-28 16:07:43 +0530

    welcome video added

--- static/index.js
+++ static/index.js
@@ -18,3 +18,40 @@ function openForm1() {
     document.getElementById("myForm2").style.display = "none";
     document.getElementById("mycontainer").style.filter = "blur(0)";
   }
+  const btnE1 = document.querySelector(".mybtn");
+  const closeIconE1 = document.querySelector(".close-icon");
+  const trailerContainerE1 = document.querySelector(".trailercontainer");
+  const ContainerE1 = document.querySelector(".container1");
+  const ContainerE2 = document.querySelector(".container2");
+  const ContainerE3 = document.querySelector(".container3");
+  const ContainerE4 = document.querySelector(".container4");
+  const ContainerE5 = document.querySelector(".container5");
+  btnE1.addEventListener("click", ()=>{
+    trailerContainerE1.classList.remove("active");
+    
+    ContainerE1.classList.add("active2");
+    ContainerE2.classList.add("active2");
+    ContainerE3.classList.add("active2");
+    ContainerE4.classList.add("active2");
+    ContainerE5.classList.add("active2");
+});
+const videos = document.querySelectorAll('iframe')
+
+
+closeIconE1.addEventListener('click', () => {
+   videos.forEach(i => {
+      const source = i.src
+      i.src = ''
+      i.src = source
+   })
+});
+closeIconE1.addEventListener("click",()=>{
+  trailerContainerE1.classList.add("active");
+
+  ContainerE1.classList.remove("active2");
+  ContainerE2.classList.remove("active2");
+  ContainerE3.classList.remove("active2");
+  ContainerE4.classList.remove("active2");
+  ContainerE5.classList.remove("active2");
+  
+});

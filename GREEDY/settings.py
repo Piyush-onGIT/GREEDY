bfd24a1d27b2446f997ed7522ed598cf3610d617
Piyush Kumar <piyush.at.googl@gmail.com>
2022-11-12 22:37:50 +0530

    ready to deploy on vercel

--- GREEDY/settings.py
+++ GREEDY/settings.py
@@ -76,10 +76,10 @@ WSGI_APPLICATION = 'GREEDY.wsgi.application'
 # https://docs.djangoproject.com/en/4.0/ref/settings/#databases
 
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
+    # 'default': {
+    #     'ENGINE': 'django.db.backends.sqlite3',
+    #     'NAME': BASE_DIR / 'db.sqlite3',
+    # }
 }
 
 
